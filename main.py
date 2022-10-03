30731994bd696a0341df8ecf34c2a426525e3289
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2022-10-02 22:37:48 -0500

    Added ephemeral to all the responses and added a presence

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ token = 'PUT TOKEN HERE'
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name='Retime Bot | Made by Conner#4200'))
     print(f'{bot.user.name} is ready!')
 
 def format(time):
@@ -37,56 +38,56 @@ async def frame(ctx, start: discord.Option(int, "Start Time",), end: discord.Opt
     try:
         time = str(round((end - start) / framerate, 3))
     except:
-        await ctx.respond('Error: An Unkown Error Occured')
+        await ctx.respond('Error: An Unkown Error Occured', ephemeral = True)
     try:
         formatted_time = format(time)
     except:
-        await ctx.respond('Error: An Unkown Error Occured')
+        await ctx.respond('Error: An Unkown Error Occured', ephemeral = True)
     embed = discord.Embed(
         title = 'Final Time', 
         description = f'Your Final time is: {formatted_time}',
         color = 0x00ff00)
-    await ctx.respond(embed = embed)
+    await ctx.respond(embed = embed, ephemeral = True)
 
 @bot.slash_command(name = 'debug_info', description = 'Retime with the Debug Info')
 async def debug_info(ctx, start: discord.Option(str, "Start Time",), end: discord.Option(str, "End Time"), framerate: discord.Option(int, "Framerate")):
     try:
         start_dict = loads(start)
     except:
-        await ctx.respond('Error: Invalid Start Debug Info')
+        await ctx.respond('Error: Invalid Start Debug Info', ephemeral = True)
         return
     try:
         end_dict = loads(end)
     except:
-        await ctx.respond('Error: Invalid End Debug Info')
+        await ctx.respond('Error: Invalid End Debug Info', ephemeral = True)
         return
     try:
         start_cmt = d(start_dict['cmt'])
     except:
-        await ctx.respond('Error: Invalid Start Debug Info')
+        await ctx.respond('Error: Invalid Start Debug Info', ephemeral = True)
         return
     try:
         end_cmt = d(end_dict['cmt'])
     except:
-        await ctx.respond('Error: Invalid End Debug Info')
+        await ctx.respond('Error: Invalid End Debug Info', ephemeral = True)
         return
     try:
         framerate = int(framerate)
     except:
-        await ctx.respond('Error: Invalid Framerate')
+        await ctx.respond('Error: Invalid Framerate', ephemeral = True)
     try:
         time = str(round((d(end_cmt) - d(start_cmt)) - (d(end_cmt) - d(start_cmt)) % (d(1)/int(framerate)), 3))
     except:
-        await ctx.respond('Error: An Unkown Error Occured')
+        await ctx.respond('Error: An Unkown Error Occured', ephemeral = True)
         print('Alert: An Unkown Error Occured')
     try:
         formatted_time = format(time)
     except:
-        print('Alert: An Unkown Error Occured on Line 73')
+        await ctx.respond('Error: An Unkown Error Occured', ephemeral = True)
     embed = discord.Embed(
         title = 'Final Time', 
         description = f'Your Final time is: {formatted_time}',
         color = 0x00ff00)
-    await ctx.respond(embed = embed)
+    await ctx.respond(embed = embed, ephemeral = True)
     
 bot.run(token)
